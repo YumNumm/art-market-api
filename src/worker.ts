472635dc7d72d01d4d8629c714928aad1d890464
YumNumm <onory0917@gmail.com>
2023-08-13 15:31:44 +0900

    add: api

--- src/worker.ts
+++ src/worker.ts
@@ -1,32 +1,93 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { Bindings } from './bindings';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	BUCKET: R2Bucket ;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+app.notFound(async (c) => c.json({ erorr: 'not found' }, 404));
+
+app.get('/list', async (c) => {
+	const result = await c.env.BUCKET.list();
+	return c.json({ result });
+});
+
+// upload picture
+app.put('/upload/:id', async (c) => {
+	try {
+		// query parameter
+		const id = c.req.param('id');
+		const name = c.req.query('name');
+		if (id === undefined) {
+			return c.json({ error: 'id is required' }, 400);
+		}
+		if (name === undefined) {
+			return c.json({ error: 'name is required' }, 400);
+		}
+
+		// upload file
+		const body = await c.req.parseBody();
+		const file = body.file as File;
+		const bucket = c.env.BUCKET;
+		const fileName = `${id}/${name}`;
+		const response = await bucket.put(fileName, await file.arrayBuffer(), {
+			httpMetadata: {
+				contentType: file.type,
+			},
+		});
+		return c.json({
+			message: `upload ${fileName} success`,
+			object: response,
+			fileName,
+		});
+	} catch (error) {
+		return c.json({ error: error }, 500);
+	}
+});
+
+// get file lists in bucket filtered by id
+app.get('/list/:id', async (c) => {
+	try {
+		const id = c.req.param('id');
+		if (id === undefined) {
+			return c.json({ error: 'id is required' }, 400);
+		}
+		const bucket = c.env.BUCKET;
+		bucket.list({
+			prefix: id,
+		});
+		const result = await bucket.list({});
+		return c.json({ result });
+	} catch (error) {
+		return c.json({ error: error }, 500);
+	}
+});
+
+// get file
+app.get('/file/:id/:name', async (c) => {
+	try {
+		const id = c.req.param('id');
+		const name = c.req.param('name');
+		if (id === undefined) {
+			return c.json({ error: 'id is required' }, 400);
+		}
+		if (name === undefined) {
+			return c.json({ error: 'name is required' }, 400);
+		}
+		const bucket = c.env.BUCKET;
+		const fileName = `${id}/${name}`;
+		const object = await bucket.get(fileName);
+		if (object === null) {
+			return c.json({ error: 'file not found' }, 404);
+		}
+		const headers = new Headers();
+		object.writeHttpMetadata(headers);
+		headers.set('etag', object.etag);
+
+		return new Response(object.body, {
+			headers: headers,
+		});
+	} catch (error) {
+		return c.json({ error: error }, 500);
+	}
+});
+
+export default app;
